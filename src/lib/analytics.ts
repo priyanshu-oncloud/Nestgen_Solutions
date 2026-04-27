a83337598a4ca83146de0a86a48939c587ce5382
priyanshu-oncloud <priyanshugoyal8839@gmail.com>
2026-04-27 20:35:11 +0530

    add multiple files

--- src/lib/analytics.ts
+++
@@ -1,7 +0,0 @@
-import { getAnalytics } from "firebase/analytics";
-import { app } from "../firebase";
-
-export const loadAnalytics = async () => {
-  if (typeof window === "undefined") return;
-  return getAnalytics(app);
-};
